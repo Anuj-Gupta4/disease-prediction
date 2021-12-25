b21a12d0bdf5b7dcf3be6ce3360d71e633f7766d
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2021-12-25 19:26:47 +0545

    add files

---
+++ DiseasePredictor-master/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request,url_for
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+dic = {0 : 'Cat', 1 : 'Dog'}
+
+model = load_model('tb_model.h5')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(100,100))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 100,100,3)
+	p = model.predict_classes(i)
+	return dic[p[0]]
+
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/heart")
+def heart():
+	return render_template("heart.html")
+
+@app.route("/tb")
+def tb():
+	return render_template("tb.html")
+
+@app.route("/about")
+def about_page():
+	return "Bharat Anuj Ayush Bhoopesh..!!!"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+@app.route("/heart_predict", methods=['GET','POST'])
+def heart_predict():
+	if request.method == 'POST':
+		age = float(request.form['age'])
+		sex = float(request.form['sex'])
+		cp = float(request.form['cp'])
+		trestbps = float(request.form['trestbps'])
+		chol = float(request.form['chol'])
+		fbs= float(request.form['fbs'])
+		restecg = float(request.form['restecg'])
+		thalach = float(request.form['thalach'])
+		exang = float(request.form['exang'])
+		oldpeak = float(request.form['oldpeak'])
+		slope = float(request.form['slope'])
+		ca = float(request.form['ca'])
+		thal = float(request.form['thal'])
+
+		pred_args = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+
+		mul_reg = open('heart_model.pkl','rb')
+		ml_model = pickle.load(mul_reg)
+		model_predcition = ml_model.predict([pred_args])
+		res = "some default value to avoid error"
+		
+
+
+		if model_predcition == 1:
+			res = 'Affected'
+		else:
+			res = 'Not affected'
+		#return res
+	return render_template('heart_predict.html', prediction = res)
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run()
---
+++ DiseasePredictor-master/readme.md
@@ -0,0 +1,5 @@
+<h1>this is disease predictor webapp
+   💡
+You can check the website deployed at Heroku here :<br>
+https://disease-predictor-webapp.herokuapp.com/tb
+   </h1>
